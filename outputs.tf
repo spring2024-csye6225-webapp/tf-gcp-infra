2973654d66f15d7d8b9edb2a5d61a4fccb5c7329
abhaydee <abhaygirish1997@gmail.com>
2024-02-12 17:51:50 -0500

    adding the variables and the output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "terraform_output" {
   value = "All the neccessary network resouces are now setup"
 }
+
+output "webapp_subnet_cidr" {
+  value = google_compute_subnetwork.webapp_subnet.ip_cidr_range
+}
+
+output "db_subnet_cidr" {
+  value = google_compute_subnetwork.db_subnet.ip_cidr_range
+}
