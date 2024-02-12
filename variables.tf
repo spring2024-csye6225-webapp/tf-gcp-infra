a716b9c161c4a503188485bd64a0e465bf3b2153
abhaydee <abhaygirish1997@gmail.com>
2024-02-12 17:51:50 -0500

    adding the variables and the output

--- variables.tf
+++ variables.tf
@@ -5,3 +5,49 @@ variable "project_id" {
 variable "region" {
   description = "Region to run the project"
 }
+
+
+variable "webapp_subnet_cidr" {
+  description = "CIDR address range for webapp subnet"
+}
+
+variable "db_subnet_cidr" {
+  description = "CIDR address range for db subnet"
+}
+
+
+variable "webapp_destination_route" {
+  description = "Destination range"
+}
+
+variable "webapp_route_priority" {
+  description = "priority value"
+}
+
+variable "vpc_name" {
+  description = "VPC network name"
+}
+
+
+variable "webapp_subnet_name" {
+  description = "webapp subnet name"
+}
+
+
+variable "db_subnet_name" {
+  description = "db subnet name"
+}
+
+
+variable "webapp_route_name" {
+  description = "webapp route name"
+}
+
+variable "hop_gateway_value" {
+  description = "hop gateway value"
+}
+
+
+variable "service_key_route" {
+  description = "service key file route"
+}
