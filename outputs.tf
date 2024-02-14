c69611c5ea7884944096dddc2804a5bed39d24c2
abhaydee <abhaygirish1997@gmail.com>
2024-02-14 17:59:42 -0500

    terraform changes for multiple vpc
    committing changes

--- outputs.tf
+++ outputs.tf
@@ -15,9 +15,13 @@ output "db_subnet_cidr" {
 }
 
 output "webapp_subnet_cidr" {
-  value = google_compute_subnetwork.webapp_subnet.ip_cidr_range
+  value = {
+    for idx, subnet in google_compute_subnetwork.webapp_subnet : idx => subnet.ip_cidr_range
+  }
 }
 
 output "db_subnet_cidr" {
-  value = google_compute_subnetwork.db_subnet.ip_cidr_range
+  value = {
+    for idx, subnet in google_compute_subnetwork.db_subnet : idx => subnet.ip_cidr_range
+  }
 }
