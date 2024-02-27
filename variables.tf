1bb90f3a6f68b2de59f42810a645e455ee68f0f7
abhaydee <abhaygirish1997@gmail.com>
2024-02-26 19:27:48 -0500

    changes to terraform cloud instance

--- variables.tf
+++ variables.tf
@@ -1,68 +1,91 @@
 variable "project_id" {
-  description = "ID of the google cloud project"
+  description = "ID of the Google Cloud project"
 }
 
 variable "region" {
-  description = "Region to run the project"
+  description = "Region where the resources will be deployed"
 }
 
-
-# variable "webapp_subnet_cidr" {
-#   description = "CIDR address range for webapp subnet"
-# }
-
-# variable "db_subnet_cidr" {
-#   description = "CIDR address range for db subnet"
-# }
-
-
 variable "webapp_destination_route" {
-  description = "Destination range"
+  description = "Destination range for the webapp route"
 }
 
 variable "webapp_route_priority" {
-  description = "priority value"
+  description = "Priority value for the webapp route"
 }
 
 variable "vpc_name" {
-  description = "VPC network name"
+  description = "Name of the VPC network"
 }
-
 
 variable "webapp_subnet_name" {
-  description = "webapp subnet name"
+  description = "Name of the subnet for the web application"
 }
-
 
 variable "db_subnet_name" {
-  description = "db subnet name"
+  description = "Name of the subnet for the database"
 }
 
-
 variable "webapp_route_name" {
-  description = "webapp route name"
+  description = "Name of the route for the web application"
 }
 
 variable "hop_gateway_value" {
-  description = "hop gateway value"
+  description = "Next hop gateway value for the route"
 }
-
 
 variable "service_key_route" {
-  description = "service key file route"
+  description = "File route for the Google Cloud service account key"
 }
 
-
 variable "vpc_count" {
-  description = "No of VPC's that has to be created"
+  description = "Number of VPCs to be created"
   type        = number
 }
 
-
 variable "zone" {
-  description ="zone"
+  description = "Zone where the instances will be created"
 }
 
-variable "image_name" { 
-  description = "image name"
+variable "image_name" {
+  description = "Name of the image to be used for boot disk"
+}
+
+
+variable "database_name" {
+  description = "Name of the database"
+}
+
+variable "database_user_name" {
+  description = "Name of the database user"
+}
+
+
+variable "cloudsql_instance_name" {
+  description = "Name of cloud sql instance"
+}
+
+
+variable "deletion_protection" {
+  description = "Deletion protection toggle for the cloud sql instance"
+}
+
+variable "availability_type" {
+  default     = "REGIONAL"
+  description = "regional or global location from the google cloud sql instance can be accessed"
+}
+
+variable "disk_type" {
+  default     = "pd-ssd"
+  description = "Type of disk to be used for the database"
+}
+
+variable "disk_size" {
+  default     = 10
+  description = "Size of the disk to be used"
+}
+
+variable "ipv4_enabled" {
+  default     = false
+  description = "ipv4 enablement toggle"
 }
