42f0a7c20f88ce2e8e96e3eba06ffa75d3926ab3
abhaydee <abhaygirish1997@gmail.com>
2024-02-29 00:30:55 -0500

    changes to tf instances

--- main.tf
+++ main.tf
@@ -1,29 +1,28 @@
 provider "google" {
-  credentials = file(var.service_key_route)
-  project     = var.project_id
-  region      = var.region
+  project = var.project_id
+  region  = var.region
 }
 
 resource "google_compute_network" "vpc_network" {
-  count                   = var.vpc_count
-  name                    = "${var.vpc_name}-${count.index}"
-  auto_create_subnetworks = false
-
+  count                           = var.vpc_count
+  name                            = "${var.vpc_name}-${count.index}"
+  auto_create_subnetworks         = false
+  delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp_subnet" {
-  count                    = var.vpc_count
-  name                     = "${var.webapp_subnet_name}-${count.index}"
-  ip_cidr_range            = var.webapp_subnet_cidr[count.index]
-  network                  = google_compute_network.vpc_network[count.index].self_link
-  private_ip_google_access = true // access for private ip's
+  count         = var.vpc_count
+  name          = "${var.webapp_subnet_name}-${count.index}"
+  ip_cidr_range = var.webapp_subnet_cidr[count.index]
+  network       = google_compute_network.vpc_network[count.index].self_link
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
-  count         = var.vpc_count
-  name          = "${var.db_subnet_name}-${count.index}"
-  ip_cidr_range = var.db_subnet_cidr[count.index]
-  network       = google_compute_network.vpc_network[count.index].self_link
+  count                    = var.vpc_count
+  name                     = "${var.db_subnet_name}-${count.index}"
+  ip_cidr_range            = var.db_subnet_cidr[count.index]
+  network                  = google_compute_network.vpc_network[count.index].self_link
+  private_ip_google_access = true
 }
 
 resource "google_compute_route" "webapp_route" {
@@ -55,87 +54,10 @@ variable "db_subnet_cidr" {
   description = "CIDR address range for db subnets"
   type        = list(string)
 }
-
-//googlecomputeglobaladdress  create an IP 
-resource "google_compute_global_address" "instance_ip" {
-  name         = "instance-ip"
-  project      = var.project_id
-  purpose      = "EXTERNAL"
-  address_type = "EXTERNAL"
-}
-
-resource "google_service_networking_connection" "private_connection" {
-  network                 = google_compute_network.vpc_network[0].name
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.instance_ip.address]
-}
-
-
-// output - name 
-resource "google_project_service" "service_networking" {
-  project = var.project_id
-  service = "servicenetworking.googleapis.com"
-}
-
-
-resource "google_sql_database_instance" "cloud_instance" {
-  count               = var.vpc_count
-  name                = "${var.cloudsql_instance_name}-${count.index}"
-  database_version    = "POSTGRES_13"
-  region              = var.region
-  deletion_protection = var.deletion_protection
-  project             = var.project_id
-
-
-  settings {
-    tier              = "db-f1-micro"
-    availability_type = var.availability_type
-    disk_type         = var.disk_type
-    disk_size         = var.disk_size
-
-    ip_configuration {
-      ipv4_enabled    = var.ipv4_enabled
-      private_network = google_compute_network.vpc_network[count.index].self_link
-
-    }
-  }
-
-  depends_on = [
-    google_compute_network.vpc_network,
-    google_compute_subnetwork.db_subnet
-  ]
-}
-
-
-resource "google_sql_database" "postgresql" {
-  count     = var.vpc_count
-  name      = var.database_name
-  instance  = google_sql_database_instance.cloud_instance[count.index].name
-  charset   = "utf8"
-  collation = "utf8_general_ci"
-}
-
-
-resource "google_sql_user" "database_user" {
-  count    = var.vpc_count
-  name     = var.database_user_name
-  instance = google_sql_database_instance.cloud_instance[count.index].name
-  password = random_password.generated_password[count.index].result
-}
-
-resource "random_password" "generated_password" {
-  count   = var.vpc_count
-  length  = 16
-  special = true
-}
-
-
-
-
 resource "google_compute_firewall" "allow_ssh" {
   count   = var.vpc_count
-  name    = "allow-ssh-${count.index}"
-  network = google_compute_network.vpc_network[count.index].name
+  name    = "allow-ssh-${0}"
+  network = google_compute_network.vpc_network[0].name
 
   allow {
     protocol = "tcp"
@@ -146,9 +68,76 @@ resource "google_compute_firewall" "allow_ssh" {
   target_tags   = ["webapp-subnet-0"]
 }
 
+
+//googlecomputeglobaladdress  create an IP 
+resource "google_compute_global_address" "instance_ip" {
+  name          = "instance-ip"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  network       = google_compute_network.vpc_network[0].self_link
+  prefix_length = 16
+}
+resource "google_project_service" "service_networking" {
+  project = var.project_id
+  service = "servicenetworking.googleapis.com"
+}
+
+resource "google_service_networking_connection" "private_connection" {
+  network                 = google_compute_network.vpc_network[0].name
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.instance_ip.name]
+  deletion_policy         = "ABANDON"
+  depends_on = [
+    google_project_service.service_networking,
+    google_compute_network.vpc_network
+  ]
+}
+
+
+// output - name 
+
+
+
+resource "google_sql_database_instance" "cloud_instance" {
+  name                = var.cloudsql_instance_name
+  database_version    = "POSTGRES_14"
+  region              = var.region
+  deletion_protection = var.deletion_protection
+  project             = var.project_id
+  depends_on          = [google_service_networking_connection.private_connection, google_compute_network.vpc_network]
+  settings {
+    tier              = "db-f1-micro"
+    availability_type = var.availability_type
+    disk_type         = var.disk_type
+    disk_size         = var.disk_size
+
+    ip_configuration {
+      ipv4_enabled                                  = var.ipv4_enabled
+      private_network                               = google_compute_network.vpc_network[0].self_link
+      enable_private_path_for_google_cloud_services = true
+    }
+  }
+}
+
+resource "google_sql_database" "cloud-database" {
+  name     = var.database_name
+  instance = google_sql_database_instance.cloud_instance.name
+
+}
+
+resource "google_sql_user" "database_user" {
+  name     = var.database_user_name
+  instance = google_sql_database_instance.cloud_instance.name
+  password = random_password.generated_password.result
+}
+
+resource "random_password" "generated_password" {
+  length  = 16
+  special = false
+}
+
 resource "google_compute_instance" "vm_instance" {
-  count        = var.vpc_count
-  name         = "vm-instance-${count.index}"
+  name         = "vm-instance"
   machine_type = "n1-standard-1"
   zone         = var.zone
 
@@ -160,33 +149,58 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 
-  //
   metadata_startup_script = <<-EOF
-      #!/bin/bash
-      # Script to set up environment variables with database credentials
+    cd /opt/csye6225
 
-      # Database credentials
-      DB_HOST="${google_sql_database_instance.cloud_instance[count.index].ip_address}"
-      DB_USER="${var.database_user_name}"
-      DB_PASS="${random_password.generated_password[count.index].result}"
-      DB_NAME="${var.database_name}"
+    # Check if .env file exists
+    if [ -f .env ]; then
+        # If .env file exists, remove it
+        rm -f .env
+    fi
 
-      # Write credentials to .env file in /tmp directory
-      cat <<EOT >> /tmp/.env
-      DB_HOST=\${DB_HOST}
-      DB_USER=\${DB_USER}
-      DB_PASS=\${DB_PASS}
-      DB_NAME=\${DB_NAME}
-EOT
+    # Create a new .env file
+    touch .env
+
+    # Function to check if a key-value pair already exists in the .env file
+    function key_value_exists {
+        local key="$1"
+        local value="$2"
+        grep -q "^$key=" .env && grep -q "^$key=$value" .env
+    }
+
+    # Append unique key-value pairs to the .env file
+    append_to_env() {
+        local key="$1"
+        local value="$2"
+        if ! key_value_exists "$key" "$value"; then
+            echo "$key=$value" >> .env
+        fi
+    }
+
+    # Append unique key-value pairs to the .env file
+    append_to_env "POSTGRES_HOST" "${google_sql_database_instance.cloud_instance.ip_address.0.ip_address}"
+    append_to_env "POSTGRES_DB" "${var.database_name}"
+    append_to_env "POSTGRES_USER" "${var.database_user_name}"
+    append_to_env "POSTGRES_PASSWORD" "${random_password.generated_password.result}"
+
+    # Reload systemd daemon to apply changes
+
+    sudo sed -i 's/host    all             all             127.0.0.1\/32            ident/host    all             all             127.0.0.1\/32            password/g' /var/lib/pgsql/data/pg_hba.conf
+    sudo sed -i 's/host    all             all             ::1\/128                 ident/host    all             all             ::1\/128                 password/g' /var/lib/pgsql/data/pg_hba.conf
+    sudo systemctl daemon reload
 EOF
 
+
   network_interface {
-    network    = google_compute_network.vpc_network[count.index].self_link
-    subnetwork = google_compute_subnetwork.webapp_subnet[count.index].self_link
+    network    = google_compute_network.vpc_network[0].self_link
+    subnetwork = google_compute_subnetwork.webapp_subnet[0].self_link
     access_config {
       // Ephemeral public IP
     }
   }
-  tags = ["webapp-subnet-0"]
 
+  tags = ["webapp-subnet-0"]
 }
+
+
+# echo "DB_HOST=${google_sql_database_instance.cloud_instance.ip_address}" >> .env
